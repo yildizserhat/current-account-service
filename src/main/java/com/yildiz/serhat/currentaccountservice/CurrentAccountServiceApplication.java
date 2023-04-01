e358923b3669e7518161ede845f269674b95d2cf
serhatyildiz <yildiz_serhat@hotmail.com>
2023-04-01 19:09:20 +0200

    Initial commit

---
+++ src/main/java/com/yildiz/serhat/currentaccountservice/CurrentAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.serhat.currentaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yildiz/serhat/currentaccountservice/CurrentAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildiz.serhat.currentaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
